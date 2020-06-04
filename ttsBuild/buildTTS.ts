687a5eecfdda067e8d8432d6068c0ba72b7551df
Aaron Aichlmayr <waterfoul@gmail.com>
2020-06-03 22:30:25 -0500

    Build fixes

--- ttsBuild/buildTTS.ts
+++ ttsBuild/buildTTS.ts
@@ -1,6 +1,7 @@
 import { buildTTSLua } from "./buildTTSLua";
 import { readFile } from "./util";
 import fs from "fs";
+import bson from "bson";
 
 Promise.all([buildTTSLua(), readFile("./tts/save.json")]).then(
   ([scripts, save]) => {
@@ -20,14 +21,25 @@ Promise.all([buildTTSLua(), readFile("./tts/save.json")]).then(
         object.XmlUI = other[v][1];
       }
     });
-    return new Promise((res, rej) => {
-      fs.writeFile("build/save.json", JSON.stringify(saveData), (err) => {
-        if (err) {
-          rej(err);
-        } else {
-          res();
-        }
-      });
-    });
+    return Promise.all([
+      new Promise((res, rej) => {
+        fs.writeFile("build/save.json", JSON.stringify(saveData), (err) => {
+          if (err) {
+            rej(err);
+          } else {
+            res();
+          }
+        });
+      }),
+      new Promise((res, rej) => {
+        fs.writeFile("build/save.bson", bson.serialize(saveData), (err) => {
+          if (err) {
+            rej(err);
+          } else {
+            res();
+          }
+        });
+      }),
+    ]);
   }
 );
