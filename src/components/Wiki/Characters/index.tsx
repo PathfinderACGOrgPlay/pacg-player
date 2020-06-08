1b2b9053b433231e2d710cf26d0507000db9a587
Aaron Aichlmayr <waterfoul@gmail.com>
2020-06-07 20:03:47 -0500

    Cleanup

--- src/components/Wiki/Characters/index.tsx
+++ src/components/Wiki/Characters/index.tsx
@@ -2,13 +2,11 @@ import { Route } from "react-router";
 import React from "react";
 import { Home } from "./Home";
 import { Edit } from "./Edit";
-import { ImportCharacters } from "../../../oldData/ImportCharacters";
 import { Character } from "./Character";
 
 export function Characters() {
   return (
     <>
-      <ImportCharacters />
       <Route exact path="/wiki/characters" component={Home} />
       <Route
         exact
